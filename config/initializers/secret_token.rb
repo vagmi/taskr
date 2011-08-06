9e83dcb8146aea96eedf14aa6ea24ec5c1e2db5e
vagmi <vagmi.mudumbai@gmail.com>
2011-08-06 10:17:23 +0530

    a blank rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskr::Application.config.secret_token = '932ee75692762f4d3f21b1fcf9585061c42d5fc9c3bce24675e72e6d9b966230e4b5b2969988b89b51cf4d978ca0dc6cbd2b334ff752e11fbc5bf43ff793d0a5'
