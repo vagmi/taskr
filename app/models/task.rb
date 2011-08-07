784b1358d40edadfbb8b898071d42a8b0f80ea19
vagmi <vagmi.mudumbai@gmail.com>
2011-08-07 11:11:20 +0530

    add support for tagging from the UI

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@ class Task < ActiveRecord::Base
   belongs_to :bucket
   has_many :taggables
   has_many :tags, :through=>:taggables
+  #before_save :set_tags
   def complete!
     self.done=true
     self.save!
@@ -11,5 +12,39 @@ class Task < ActiveRecord::Base
   end
   def display
     "#{self.title} #{to_hashtags}"
+  end
+
+  #def set_tags
+    #puts 'set tags called'
+    #pos = (self.title=~/#\w+/)
+    #if(pos)
+      #tags=self.title.scan(/#\w+/)
+      #self.title=self.title.slice(0,pos-1)
+      #tags.each do |t|
+        #tag=Tag.find_by_name(t)
+        #tag=Tag.new(:name=>t) unless tag
+        #self.tags<<tag
+      #end
+    #end
+  #end
+
+  def title
+    "#{super} #{self.tags.map(&:name).join(" ")}"
+  end
+
+  # faux accessor pattern
+  def title=(value)
+    pos = (value=~/#\w+/)
+    if(pos)
+      super(value.slice(0,pos-1))
+      tags=value.scan(/#\w+/)
+      tags.each do |t|
+        tag=Tag.find_by_name(t)
+        tag=Tag.new(:name=>t) unless tag
+        self.tags<<tag
+      end
+    else
+      super(value)
+    end
   end
 end
